fe558e2ee4017728e5613d1140a896c652db4b28
benIoannidis <ben.april95@gmail.com>
2024-07-31 09:40:53 +1000

    Ben - swapped raycast for show controls to collider trigger

--- 333/Assets/UI/Animators/ControlsInfo.cs
+++ 333/Assets/UI/Animators/ControlsInfo.cs
@@ -8,28 +8,57 @@ public class ControlsInfo : MonoBehaviour
     public Animator lhAnimator;
     public Transform headTransform;
 
-    private void Update()
-    {
-        Ray ray = new Ray(headTransform.position, headTransform.forward);
-        RaycastHit hit;
+    //private void Update()
+    //{
+    //    Ray ray = new Ray(headTransform.position, headTransform.forward);
+    //    RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit))
+    //    if (Physics.Raycast(ray, out hit))
+    //    {
+    //        if (hit.collider.tag == "RHController")
+    //        {
+    //            rhAnimator.SetBool("LookingAtController", true);
+    //            lhAnimator.SetBool("LookingAtController", false);
+    //        }
+    //        else if (hit.collider.tag == "LHController")
+    //        {
+    //            rhAnimator.SetBool("LookingAtController", false);
+    //            lhAnimator.SetBool("LookingAtController", true);
+    //        }
+    //        else
+    //        {
+    //            rhAnimator.SetBool("LookingAtController", false);
+    //            lhAnimator.SetBool("LookingAtController", false);
+    //        }
+    //    }
+    //}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "RHController")
         {
-            if (hit.collider.tag == "RHController")
-            {
-                rhAnimator.SetBool("LookingAtController", true);
-                lhAnimator.SetBool("LookingAtController", false);
-            }
-            else if (hit.collider.tag == "LHController")
-            {
-                rhAnimator.SetBool("LookingAtController", false);
-                lhAnimator.SetBool("LookingAtController", true);
-            }
-            else
-            {
-                rhAnimator.SetBool("LookingAtController", false);
-                lhAnimator.SetBool("LookingAtController", false);
-            }
+            rhAnimator.SetBool("LookingAtController", true);
+            lhAnimator.SetBool("LookingAtController", false);
+        }
+        else if (other.tag == "LHController")
+        {
+            rhAnimator.SetBool("LookingAtController", false);
+            lhAnimator.SetBool("LookingAtController", true);
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "RHController")
+        {
+            rhAnimator.SetBool("LookingAtController", false);
+            lhAnimator.SetBool("LookingAtController", true);
+        }
+        else if (other.tag == "LHController")
+        {
+            rhAnimator.SetBool("LookingAtController", true);
+            lhAnimator.SetBool("LookingAtController", false);
         }
     }
 }
